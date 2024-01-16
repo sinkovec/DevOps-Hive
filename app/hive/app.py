47f9397853f711c9da73a360ffdfdcd511d26dec
Brian Sinkovec <bsin1807@gmail.com>
2024-01-16 22:43:15 +0100

    feat: :sparkles: Add version controller method to hive app

--- app/hive/app.py
+++ app/hive/app.py
@@ -1,7 +1,22 @@
+"""
+Main entrypoint of the hive app.
+"""
 import importlib.metadata
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
 def version():
+    """
+    Returns the current version of the deployed app.
+    """
     return importlib.metadata.version("hive")
 
-def start():
-    print(version())
+
+@app.get("/version")
+def read_version():
+    """
+    GET method to return the current app version.
+    """
+    return version()
