aef6a5054673b8afd98119a29710fa00ebdf9310
Brian Sinkovec <bsin1807@gmail.com>
2024-01-20 15:14:26 +0100

    feat(k8s): :bricks: Add kind config and setup with ingress-nginx

---
+++ k8s/kind-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# create kind cluster from config
+kind create cluster --config=kind-cluster.yaml
+
+# setup ingress-nginx
+echo "Apply nginx-ingres static manifests"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+echo "Wait until ingress-nginx is ready.."
+sleep 20
+# wait until its ready
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+echo "Setup complete. Run kind delete cluster -n hive-cluster to cleanup"
