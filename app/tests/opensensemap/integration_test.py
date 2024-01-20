50863255971e0a7f51f5ef6f1a051db2f69a4c43
Brian Sinkovec <bsin1807@gmail.com>
2024-01-20 16:27:24 +0100

    test(app): :white_check_mark: Add integration test for temperature endpoint

---
+++ app/tests/opensensemap/integration_test.py
@@ -0,0 +1,54 @@
+"""
+Module: test_open_sense_map_api.py
+
+This module contains integration tests for the OpenSenseMap API endpoint in the hive.app module.
+"""
+from datetime import datetime, timezone
+from fastapi.testclient import TestClient
+import pytest
+
+from hive.app import app
+
+client = TestClient(app)
+
+@pytest.fixture(name="fake_sensor_data")
+def fixture_fake_sensor_data():
+    """
+    Fixture to provide fake sensor data for testing.
+
+    Returns:
+        dict: Dictionary representing fake sensor data.
+    """
+    return {
+        "_id": "1",
+        "title": "Sensor title",
+        "lastMeasurement": {
+            "createdAt": datetime.now(timezone.utc).isoformat(),
+            "value": "10"
+        }
+    }
+
+
+def test_temperature(mocker, fake_sensor_data):
+    """
+    Test the temperature endpoint of the hive app.
+
+    Checks if the temperature endpoint returns the expected result based on fake sensor data.
+
+    Args:
+        mocker: Pytest mocker fixture for mocking requests lib.
+        fake_sensor_data: Fake sensor data provided by the fixture.
+    """
+    # given
+    fake_resp = mocker.Mock()
+    fake_resp.json.return_value = fake_sensor_data
+    fake_resp.status_code = 200
+
+    mocker.patch("hive.opensensemap.api.requests.get", return_value=fake_resp)
+
+    response = client.get("/temperature")
+
+    assert response.status_code == 200
+    content = response.json()
+    assert content["status"] == "Good"
+    assert content["temperature"] == 10
