f91cdf2df285809807da0db18cdcf0d42141f097
Brian Sinkovec <bsin1807@gmail.com>
2024-01-21 13:41:11 +0100

    feat(app): :zap: Add redis cache layer to temperature endpoint

--- app/hive/opensensemap/router.py
+++ app/hive/opensensemap/router.py
@@ -9,10 +9,13 @@ Endpoints:
         return the average temperature of sense box sensors.
 
 """
+import json
 from fastapi import APIRouter
+from redis import Redis
 
 from .config import service
 
+redis = Redis(host="localhost", port=6379)
 router = APIRouter()
 
 @router.get("/temperature")
@@ -21,10 +24,46 @@ def read_temperature():
     GET method to calculate and return the average temperature of sense box sensors.
 
     Returns:
-        float: The average temperature value of all sensors.
+        dict: Dictionary containing "status" and "temperature" keys.
+    """
+    data = get_cache()
+
+    if not data:
+        data = create_response()
+        set_cache(data)
+
+    return data
+
+def create_response():
+    """
+    Creates a response containing the status and average temperature.
+
+    Returns:
+        dict: Dictionary containing "status" and "temperature" keys.
     """
     avg_temperature = service.calculate_average_temperature()
     return {
         "status": service.temperature_status(avg_temperature),
         "temperature": avg_temperature
     }
+
+def get_cache():
+    """
+    Retrieves cached data from Redis.
+
+    Returns:
+        dict: Dictionary containing cached data or None if not found.
+    """
+    data = redis.get("opensensemap_temperature")
+    if data:
+        data = json.loads(data)
+    return data
+
+def set_cache(data):
+    """
+    Sets data into the Redis cache.
+
+    Args:
+        data: Data to be stored in the cache.
+    """
+    redis.set("opensensemap_temperature", json.dumps(data), ex=120)
