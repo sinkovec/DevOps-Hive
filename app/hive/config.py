f283ffeeebf84899502bda085ae51fbe80e5a16a
Brian Sinkovec <bsin1807@gmail.com>
2024-01-22 15:13:49 +0100

    test(app): :wrench: enable dynaconf for tests and add vscode settings for testing

--- app/hive/config.py
+++ app/hive/config.py
@@ -1,8 +1,12 @@
 """
 Dynaconf settings module.
 """
+import os
 from dynaconf import Dynaconf
 
+current_directroy = os.path.dirname(os.path.realpath(__file__))
+
 settings = Dynaconf(
+    root_path=current_directroy,
     settings_files=["settings.toml"]
 )
