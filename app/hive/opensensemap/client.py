f83d2f5b9907063cabede4e9cafafa127ea35091
Brian Sinkovec <bsin1807@gmail.com>
2024-02-07 18:31:07 +0100

    refactor(app): :recycle: Major refactoring including all opensensemap modules
    Added CachingRepository, removed DAO and DB, rename API to Client, provide SenseBox IDs through settings.toml,Implement Availability through separate service, use Pydantic for models instead of dataclasses

--- app/hive/opensensemap/client.py
+++ app/hive/opensensemap/client.py
@@ -7,6 +7,7 @@ API requests to the OpenSenseMap API.
 import requests
 
 
+# pylint: disable=too-few-public-methods
 class OpenSenseMapClient:
     """
     Class to handle API requests to OpenSenseMap API.
@@ -18,36 +19,18 @@ class OpenSenseMapClient:
     def __init__(self, base_url: str):
         self.base_url = base_url
 
-    def fetch_sensor_data(self, sense_box_id, sensor_id):
+    def fetch_sense_box(self, sense_box_id):
         """
-        Retrieves current sensor data for the given Sense Box and Sensor id
-        using the OpenSenseMap API.
+        Fetches sense box from API by id.
 
         Args:
             sense_box_id (str): Identifier for the Sense Box.
-            sensor_id (str): Identifier for the sensor.
 
         Returns:
-            list: Sensor instance representing the retrieved current sensor data.
+            SenseBox: SenseBox instance.
         """
-        response = self._get_sensor_data(sense_box_id, sensor_id)
+        response = requests.get(f"{self.base_url}/boxes/{sense_box_id}", timeout=30)
         data = None
         if response.status_code == 200:
             data = response.json()
         return data
-
-    def sensor_data_ok(self, sense_box_id, sensor_id):
-        """
-        Performs an HEAD request to the sensor endpoint to check its availability.
-
-        Returns:
-            bool: true if the response status code is ok.
-        """
-        response = self._get_sensor_data(sense_box_id, sensor_id)
-        return response.status_code == 200
-
-    def _get_sensor_data(self, sense_box_id, sensor_id):
-        return requests.get(self._sensor_url(sense_box_id, sensor_id), timeout=30)
-
-    def _sensor_url(self, sense_box_id, sensor_id):
-        return f"{self.base_url}/boxes/{sense_box_id}/sensors/{sensor_id}"
--- app/hive/opensensemap/service.py
+++ app/hive/opensensemap/service.py
@@ -1,26 +1,29 @@
 """
-Module for interacting with the OpenSenseMap service to calculate average temperatures.
+Module for interacting with the OpenSenseMap service to 
+calculate average temperatures and availability.
 
-This module defines the OpenSenseMapService class, which provides functionality for
+This module defines the OpenSenseMapTemperatureService class, which provides functionality for
 retrieving data from the OpenSenseMap repository and calculating the average temperature
 emitted by sensors in the given Sense Boxes.
+
+This module further defines the OpenSenseMapAvailabilityService class, which provides
+functionality to request whether sensors and caching content are available.
 """
 from datetime import datetime, timedelta, timezone
 from statistics import mean
 
 from .schemas import TemperatureBase, TemperatureStatus
-from .repository import OpenSenseMapRepository
 
 
-class OpenSenseMapService:
+class OpenSenseMapTemperatureService:
     """
-    OpenSenseMapService class for interacting with the OpenSenseMap service.
+    OpenSenseMapTemperatureService class for interacting with the OpenSenseMap service.
 
     This class encapsulates functionality to interact with the OpenSenseMap repository
     and calculate the average temperature emitted by sensors in the given Sense Boxes.
     """
 
-    def __init__(self, repository: OpenSenseMapRepository):
+    def __init__(self, repository):
         self.repository = repository
 
     def get_temperature(self) -> TemperatureBase:
@@ -64,10 +67,17 @@ class OpenSenseMapService:
             float: The average temperature value of all sensors.
         """
         from_date = self._past_hour_timestamp()
-        sensor_data = self.repository.get_sensor_data()
+        sense_boxes = self.repository.find_all()
+        sensors_per_sense_box = [
+            sense_box.sensors for sense_box in sense_boxes if sense_box
+        ]
+        temperature_sensors = [
+            next(sensor for sensor in sensors if "temperatur" in sensor.title.lower())
+            for sensors in sensors_per_sense_box
+        ]
         last_measurements = [
             sensor.last_measurement.value
-            for sensor in sensor_data
+            for sensor in temperature_sensors
             if sensor and from_date < sensor.last_measurement.created_at
         ]
         if not last_measurements:
@@ -83,22 +93,42 @@ class OpenSenseMapService:
         """
         return datetime.now(timezone.utc) - timedelta(hours=1)
 
-    def sensor_data_available(self) -> bool:
+
+# pylint: disable=too-few-public-methods
+class OpenSenseMapAvailabilityService:
+    """
+    OpenSenseMapAvailabilityService class to inform about the availability status
+    of OpenSenseMap sensors and caching freshness.
+    """
+
+    def __init__(self, repository, caching_repository):
+        self.repository = repository
+        self.caching_repository = caching_repository
+
+    def is_available(self) -> bool:
         """
         Check if sensor data is available based on certain conditions.
         - More than 50% + 1 sensors are available AND
-        - Caching content does not expire within 5 minutes.
+        - Caching content is not older than 5 minutes.
 
         Returns:
             bool: True if sensor data is available; False otherwise.
         """
-        sensors = self.repository.sensors_ready()
-        if sensors.count(False) >= len(sensors) // 2 + 1:
-            created_at = self.repository.get_created_at_times()
-            return not all(map(self._is_older_than(timedelta(minutes=5)), created_at))
+        sense_boxes = self.repository.find_all()
+        if sense_boxes.count(None) >= len(sense_boxes) // 2 + 1:
+            last_modified = self.caching_repository.last_modified_all()
+            return not all(
+                map(self._is_older_than(timedelta(minutes=5)), last_modified)
+            )
         return True
 
     def _is_older_than(self, td):
+        """
+        Returns a predicate which returns True if the given timestamp is older than td from now.
+
+        Returns:
+            func: Predicate for timestamps
+        """
         now = datetime.now(timezone.utc)
 
         def predicate(timestamp):
